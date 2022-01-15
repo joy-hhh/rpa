7d5eee102be729a5574cddab674baffa19ab0b47
joy-hhh <joyhhh@outlook.kr>
2022-01-15 19:48:08 +0900

    word number

--- word_number.py
+++ word_number.py
@@ -1,5 +1,6 @@
 from docxtpl import DocxTemplate
 import pandas as pd
+import numpy as np
 
 doc = DocxTemplate('note.docx')
 fiscal_year = {
@@ -8,10 +9,21 @@ fiscal_year = {
 }
 
 # Load note data from excel file
-note_data = pd.read_excel("values.xlsx")
+val = pd.read_excel("values.xlsx")
+note_data ={}
+for i in list(val.columns):
+    for j in range(len(val[i])): 
+        if np.isnan(val[i][j]):
+            continue
+        else: 
+            note_data[i+str(j)] = val[i][j]
 
 # Create context dictionary from note data file
 context = dict(zip(note_data['var'], note_data["value"]))
+exrate = {
+    'exrate' : 1185.5
+}
+context.update(exrate)
 
 for key in context:
     if context[key] < 0:
